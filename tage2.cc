6fa1cf5663c88cb465b60b9c41ade74a715c28b8
semim19 <alborzmahdi83@gmail.com>
2025-07-19 09:57:30 +0330

    predict

--- tage2.cc
+++ tage2.cc
@@ -19,9 +19,67 @@ void Tage::initialize_branch_predictor()
   }
 }
 
+unsigned long long foldHist(std::bitset<5500> GHR, int histLength, int compLength)
+{
+  unsigned long long folded = 0;
+  for (int i = 0; i < histLength; ++i) {
+    folded ^= (GHR[i] << (i % compLength));
+  }
+  return folded & ((1ull << compLength) - 1);
+}
+
+unsigned long long hash_index(champsim::address ip, std::bitset<5500> GHR, const Table& table, int numOfEntries)
+{
+  int compLength = static_cast<int>(log2(table.histLength));
+  auto pc = ip.to<unsigned long long>();
+  unsigned long long folded_history = foldHist(GHR, table.histLength, compLength);
+
+  unsigned long long index = pc ^ (pc >> 2) ^ folded_history;
+
+  return index % numOfEntries; // compLength = log2(table_size)
+}
+
+unsigned long long hash_tag(champsim::address ip, std::bitset<5500> GHR, const Table& table)
+{
+  int compLength = static_cast<int>(log2(table.histLength));
+  auto pc = ip.to<unsigned long long>();
+  unsigned long long folded_history = foldHist(GHR, table.histLength, table.tagWidth);
+
+  unsigned long long tag = pc ^ (pc >> 3) ^ (folded_history << 1);
+
+  return index % table.tagWidth; // compLength = log2(table_size)
+}
 bool Tage::predict_branch(champsim::address ip)
 {
+  int providerIdx = -1;
+  int alterIdx = -1;
+  int index_pred;
+  int alter_pred;
+  for (int i = numOfTables - 1; i >= 0; i--) {
+    unsigned long long tag = hash_tag(ip, GHR, tables[i]);
+    unsigned long long idx = hash_index(ip, GHR, tables[i], numOfEntries);
 
+    if (tables[i].entries[idx].tag == tag) {
+      if (providerIdx == -1) {
+        providerIdx = i;
+        index_pred = idx;
+      } else if (alterIdx == -1) {
+        alterIdx = i;
+        alter_pred = idx;
+        break;
+      }
+    }
+  }
+  if (providerIdx != -1) {
+    if (tables[providerIdx].entries[index_pred].predCounter != 3 ||
+        tables[providerIdx].entries[index_pred].predCounter != 4 ||
+        tables[providerIdx].entries[index_pred].uCounter != 0 ||
+        alterIdx == -1) {        //TODO if alter was many times better
+      return tables[providerIdx].entries[index_pred].predCounter >= 4;
+    } else {
+      return tables[alterIdx].entries[alter_pred].predCounter >= 4;
+    }
+  } else return biPred.predict_branch(ip);
 }
 
 void Tage::last_branch_result(champsim::address ip, champsim::address branch_target, bool taken, uint8_t branch_type)
